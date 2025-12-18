d421ca480480c40626dd96b39a8ca96a31050ead
Mathew A. K. <kramer.mathew.a@gmail.com>
2025-12-17 21:36:57 -0500

    Add routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '../components/HomeView.vue'
 import WordDetails from '../components/WordDetails.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
