77df6195e2a3852bcc90f23fe08643e6fd6e8604
Mathew A. K. <kramer.mathew.a@gmail.com>
2025-12-19 21:57:06 -0500

    Fix key bug and convert CSS

--- src/components/pitch/PitchDisplay.vue
+++ src/components/pitch/PitchDisplay.vue
@@ -12,7 +12,7 @@ defineProps<{
 
 <template>
     <div class="pitch-display" :data-pitch="pitch">
-        <template v-for="mora, i in getMorae(yomi)" :key="mora">
+        <template v-for="(mora, i) in getMorae(yomi)" :key="i">
             <template v-if="i === 0">
                 <PitchMora v-if="pitch === 1" :pitch="'peak'" :characters="mora?.length" :mora="mora" />
                 <PitchMora v-else :pitch="'start'" :characters="mora?.length" :mora="mora" />
@@ -23,86 +23,76 @@ defineProps<{
                 <PitchMora v-else :pitch="'low'" :characters="mora?.length" :mora="mora" />
             </template>
         </template>
+        <span class="sr-only"> - Pitch accent: {{ pitch }}</span>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="less">
 .pitch-display {
     position: relative;
     display: block;
     margin-bottom: 1em;
-    font-family: 'Noto Sans', Arial, sans-serif;
-}
+    font-size: 1.2em;
 
-.mora {
-    position: relative;
-    margin: 0.1em;
-}
+    .mora {
+        position: relative;
+        margin: 0.1em;
+        color: var(--mora-low);
 
-.mora::before, .mora::after {
-    content: "";
-    position: absolute;
-    top: -0.1em;
-}
+        &::before, &::after {
+            content: "";
+            position: absolute;
+            top: -0.1em;
+        }
 
-.mora::before {
-    height: 0.2em;
-    left: -0.2em;
-    width: 1.2em;
-}
+        &::before {
+            height: 0.2em;
+            left: -0.2em;
+            width: 1.2em;
+        }
 
-.mora[data-pitch="low"]::before, .mora[data-pitch="start"]::before {
-    height: 0.2em;
-    left: -0.2em;
-    width: 1.4em;
-    top: 1.5em;
-    background-color: #330D00;
-}
+        &[data-pitch="low"]::before, &[data-pitch="start"]::before {
+            height: 0.2em;
+            left: -0.2em;
+            width: 1.4em;
+            top: 1.5em;
+            background-color: var(--mora-low);
+        }
 
-.mora[data-pitch="start"]::after {
-    height: 1.75em;
-    width: 0.2em;
-    right: -0.2em;
-    background-color: #330D00;
-}
+        &[data-pitch="start"]::after {
+            height: 1.75em;
+            width: 0.2em;
+            right: -0.2em;
+            background-color: var(--mora-low);
+        }
 
-.mora[data-characters="2"]::before {
-    height: 0.2em;
-    width: 2.4em;
-}
+        &[data-characters="2"]::before {
+            height: 0.2em;
+            width: 2.4em;
+        }
 
-.mora[data-pitch="high"]::before {
-    background-color: #330D00;
-}
+        &[data-pitch="high"]::before {
+            background-color: var(--mora-low);
+        }
 
-.mora[data-pitch="peak"]::before, .mora[data-pitch="peak"]::after {
-    background-color: orangered;
-}
+        &[data-pitch="peak"]::before, &[data-pitch="peak"]::after {
+            background-color: var(--mora-peak);
+        }
 
-.mora[data-pitch="peak"]::after {
-    height: 1.75em;
-    width: 0.2em;
-    left: 1em;
-}
+        &[data-pitch="peak"]::after {
+            height: 1.75em;
+            width: 0.2em;
+            left: 1em;
+        }
 
-.mora[data-pitch="peak"][data-characters="2"]::after {
-    left: 2em;
-}
+        &[data-pitch="peak"][data-characters="2"]::after {
+            left: 2em;
+        }
 
-.mora[data-pitch="peak"] {
-    color: orangered;
-}
-
-@media (prefers-color-scheme: dark) {
-    .mora[data-pitch="high"]::before, .mora[data-pitch="start"]::after, 
-    .mora[data-pitch="low"]::before, .mora[data-pitch="start"]::before {
-        @apply bg-gray-200;
-    } 
-
-    button .mora[data-pitch="high"]::before, button .mora[data-pitch="start"]::after, 
-    button .mora[data-pitch="low"]::before, button .mora[data-pitch="start"]::before {
-        background-color: #330D00;
-    } 
+        &[data-pitch="peak"] {
+            color: var(--mora-peak);
+        }
+    }
 }
 </style>
 
